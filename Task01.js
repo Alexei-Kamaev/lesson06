27cc42a6348fff3329e5fef7f945ba077951aebe
Alexei-Kamaev <alexei-kamaev@yandex.ru>
2023-09-10 08:59:38 +0300

    первая задача

---
+++ Task01.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+
+    
+for (let i=1; i < 11; i += 1){
+    console.log(`Таблица степеней для ${i}:`)
+    for (let n=1; n < 11; n += 1) {
+console.log(`${i} в степени ${n} будет: ${i ** n}`);
+}
+}
+
+
+}
